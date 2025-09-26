a90886a8c21648469736fff1339caa44fe6c3fd9
Luciano Manenti <luciano.manenti@gmail.com>
2025-09-26 08:54:34 +0200

    credenziali hotspot

--- include/credentials.h
+++ include/credentials.h
@@ -4,4 +4,9 @@
 #define WIFI_SSID "VodafoneRibes"
 #define WIFI_PASSWORD "scheggia2000"
 
+//#define WIFI_SSID "mi10"
+//#define WIFI_SSID "galaxy"
+//#define WIFI_PASSWORD "12345678"
+
+
 #endif // CREDENTIALS_H
